ed8e6fcccd6719ce44983049164d239ddce3fb00
Jurijs Kolesnikovs <jurijs_kolesnikovs@inbox.lv>
2019-05-10 09:29:48 +0300

    Added svg icons

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="about">
     <button class="btn">More recipe</button>
+    <svg>
+      <use xlink:href="images/icons/icons.svg#logo"></use>
+    </svg>
+    <svg class="icon">
+      <use xlink:href="images/icons/icons.svg#chicken"></use>
+    </svg class="icon">
+    <svg class="icon">
+      <use xlink:href="images/icons/icons.svg#fish"></use>
+    </svg>
+    <svg class="icon">
+      <use xlink:href="images/icons/icons.svg#veg"></use>
+    </svg>
+    <svg class="icon">
+      <use xlink:href="images/icons/icons.svg#cookies"></use>
+    </svg>
+    <svg class="icon">
+      <use xlink:href="images/icons/icons.svg#clocks"></use>
+    </svg>
+    <svg class="icon">
+      <use xlink:href="images/icons/icons.svg#star"></use>
+    </svg>
   </div>
 </template>
